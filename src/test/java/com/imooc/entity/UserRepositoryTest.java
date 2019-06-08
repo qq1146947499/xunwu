b0f789367b5274d9574c61c1c0945729aa0b7f2d
qq1146947499 <1146947499@qq.com>
2019-06-08 09:45:55 +0800

    no message

---
+++ src/test/java/com/imooc/entity/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.imooc.entity;
+
+import com.imooc.ApplicationTests;
+import com.imooc.repository.UserRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositoryTest extends ApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindOne() {
+        User user = userRepository.findOne (1L);
+        Assert.assertEquals ("wali", user.getName ( ));
+    }
+}
